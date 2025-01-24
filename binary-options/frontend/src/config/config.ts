7a4d2fa4ecc4b96fa3c27bfb828569c6e778ff9b
Faulty Tolly <@faulttolerance.net>
2025-01-24 18:05:02 +0100

    add api base url

--- binary-options/frontend/src/config/config.ts
+++ binary-options/frontend/src/config/config.ts
@@ -1,5 +1,5 @@
 const config = {
-    apiBaseUrl: "http://34.76.101.17:1317",
+    apiBaseUrl: "https://rest.ra-3.rollapp.network",
     oracleContractAddress: "uod1yyca08xqdgvjz0psg56z67ejh9xms6l436u8y58m82npdqqhmmtqlwc5fc",
     binaryOptionsContractAddress: "uod1w27ekqvvtzfanfxnkw4jx2f8gdfeqwd3drkee3e64xat6phwjg0snsq2w3",
     base: "factory/osmo13s0f55s8ppwm35npn53pkndphzyctfl7gu8q9d/ubtc",
